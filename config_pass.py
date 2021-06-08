e04aa8f3729006c423c7e59f1b7d332f0675d60c
Daksh Bindal <44581586+daksh6171@users.noreply.github.com>
2021-06-08 23:49:41 +0530

    Add files via upload

---
+++ config_pass.py
@@ -0,0 +1,8 @@
+# The required account details of Google,Twilio and MySQL to be filled
+gmailid = "your gmail id"
+password = "your gmail id password"
+username = "mysql username"
+sql_password = "mysql password"
+twil_num = "twilio number"
+account_ID = "twilio account ID"
+auth_token = "twilio auth token"
